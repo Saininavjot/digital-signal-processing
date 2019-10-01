f1ceeb9ff79203fb0b7328dfe9dc9f38ad42c9c3
Saininavjot <54857033+Saininavjot@users.noreply.github.com>
2019-10-01 11:49:05 -0400

    Add files via upload

---
+++ deltafunc.m
@@ -0,0 +1,16 @@
+%%
+%function return a delta function over the domain(given by range)
+%%which must include 
+%user defines shift value
+function [vec]= deltafunc(shift,range)
+% x is a vector
+% We create an output vector of only 0 (our default value)
+leftlim=range(1);
+if leftlim<=0
+    zeroindex=abs(leftlim)+1;
+else
+    disp('invalid');
+    return;
+end
+vec=zeros(size(range)); %initialize vector to zero
+vec(zeroindex+shift)=1; % set the one index corresponding to shift
---
+++ lab2.m
@@ -0,0 +1,4 @@
+nr=-5:15;
+x=3*deltafunc(-2,nr)+2*deltafunc(0,nr)-deltafunc(3,nr)+5*deltafunc(7,nr);
+stem(nr,x);
+
